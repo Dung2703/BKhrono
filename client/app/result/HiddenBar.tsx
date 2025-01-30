1d5d9fe9b143fe749e5d21371c72aa4a44ba5abc
Tri Dung <ttdung10x@gmail.com>
2025-01-30 21:50:11 +0700

    Animation

--- client/app/result/HiddenBar.tsx
+++ client/app/result/HiddenBar.tsx
@@ -1,11 +1,22 @@
 "use client";
 import React, { useState } from 'react';
 import styles from './HiddenBar.module.css';
+import { useAutoAnimate } from '@formkit/auto-animate/react';
 
 const HiddenBar = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const [parent] = useAutoAnimate();
+
+  const handleClick = () => {
+    setIsVisible(!isVisible);
+  };
+
   return (
-    <div className={styles.hiddenBar}>
-      <div className={styles.filesContainer}>
+    <div className={`${styles.hiddenBar} ${isVisible ? styles.show : ''}`}>
+      <button className={styles.toggleButton} onClick={handleClick}>
+        {isVisible ? 'X' : 'File type'}
+      </button>
+      <div ref={parent} className={`${styles.filesContainer} ${isVisible ? styles.show : styles.hide}`}>
         <button className={styles.fileButton}>.pdf file</button>
         <button className={styles.fileButton}>.txt file</button>
         <button className={styles.fileButton}>.xlsx file</button>
