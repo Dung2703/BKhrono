c1b611f5ba3b5d2e10189c451764bdbb3f43a6e4
Truc Minh Nguyen <minhtruc103@gmail.com>
2025-03-23 10:47:40 +0700

    Hot Fix v1.25323: Logo Image

--- app/components/LogoSwitch/LogoSwitch.tsx
+++ app/components/LogoSwitch/LogoSwitch.tsx
@@ -1,3 +1,4 @@
+import { a } from 'framer-motion/client';
 import styles from './LogoSwitch.module.css';
 import ThemeToggle from './Theme_Toggle';
 import Image from 'next/image';
@@ -5,7 +6,8 @@ import Image from 'next/image';
 const LogoSwitch = () => {
   return (
     <div className={styles.container}>
-      <Image src="/logo" width={150} alt="logo"/>
+      {/* set height auto */}
+      <Image src="/logo.webp" height={100} width={100} alt="logo" style={{width: '100%', height: 'auto'}} priority/>
       <ThemeToggle/>
     </div>
   );
