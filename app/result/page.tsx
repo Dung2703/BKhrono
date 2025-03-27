a95d521be9de7947ef9ae395f31d5e2114a6e88f
nguyen875 <nguyen.nguyen2k4nker@hcmut.edu.vn>
2025-03-27 21:30:04 +0700

    contact + google analytics tracking code V2

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -6,44 +6,27 @@ import ResultDisplay from "@/app/ui/result/ResultDisplay/ResultDisplay"
 import Generate from "@/app/ui/result/GenerateBar/Generate";
 import { useState } from "react";
 import { useSchedulePriority } from "@/app/utils/hooks";
-import Head from "next/head"; // Import the Head component for including tags in <head>
 
 const ResultPage = () => {
   const [schedule, setSchedule] = useState<string[][]>(Array.from({ length: 12 }, () => Array(6).fill("-1")));
   const {schedule: schedulePriority, setters: setSchedulePriority} = useSchedulePriority();
   return (
-    <>
-      <Head>
-        {/* Google Analytics */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-E7R69QRCN3"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-E7R69QRCN3');
-            `,
-          }}
-        />
-      </Head>
-      <div className={styles.container}>
-        <div className={styles.leftSide}>
-          <div className={styles.priorityBar}>
-            <PriorityBar schedulePriority={schedulePriority} setSchedulePriority={setSchedulePriority}/>
-          </div>
-        </div>
-        <div className={styles.resultSection}>
-          <div className={styles.topBar}>
-            {/* <TopBar /> */}
-            <Generate isVisible={false} setSchedule={setSchedule} schedulePriorityStrings={schedulePriority}/> 
-          </div>
-          <div className={styles.resultDisplay}>
-            <ResultDisplay schedule={schedule} />
-          </div>
+    <div className={styles.container}>
+      <div className={styles.leftSide}>
+        <div className={styles.priorityBar}>
+          <PriorityBar schedulePriority={schedulePriority} setSchedulePriority={setSchedulePriority}/>
         </div>
       </div>
-    </>
+      <div className={styles.resultSection}>
+        <div className={styles.topBar}>
+          {/* <TopBar /> */}
+          <Generate isVisible={false} setSchedule={setSchedule} schedulePriorityStrings={schedulePriority}/> 
+        </div>
+        <div className={styles.resultDisplay}>
+          <ResultDisplay schedule={schedule} />
+        </div>
+      </div>
+    </div>
   )
 }
 
